6f6b0e8cb1ec07f556e9cd7006f1c1a836908821
g-dogg <g_dogg@xakep.ru>
2015-05-13 22:58:43 +0600

    modified:   css/main.css 	modified:   css/main.css.map 	deleted:    css/sass/.fuse_hidden0000000e00000002 	modified:   css/sass/main.sass 	modified:   findit.sublime-workspace 	renamed:    .fuse_hidden0000000a00000001 -> index.html 	modified:   js/common.js

--- js/common.js
+++ js/common.js
@@ -1,34 +1,9 @@
 $(document).ready(function() {
 
-	$(".section_header").animated("fadeInUp", "fadeOutDown");
-
-
-	//Ьутг
-	$('.menuButton .menuLink').click(function(e) {
-	e.preventDefault();
-	$(this).toggleClass('close');
-
-	if ($(".topMenu").is(":visible")) {
-			$(".topText").css("opacity", "1");
-			$(".topMenu").fadeOut(600);
-			$(".topMenu li a").removeClass("fadeInUp animated");
-		} else {
-			$(".topText").css("opacity", ".1");
-			$(".topMenu").fadeIn(600);
-			$(".topMenu li a").addClass("fadeInUp animated");
-		};
-	});
-
-		$(".topMenu ul a").click(function() {
-		$(".topMenu").fadeOut(600);
-		$(".menuLink").toggleClass("active");
-		$(".topText").css("opacity", "1");
-	}).append("<span>");
-
-	function heightDetect() {
-		$(".mainHead").css("height", $(window).height());
+	function heightDetect($percentage) {
+		$(".mainHead").css("height", $(window).height() * $percentage );
 	};
-	heightDetect();
+	heightDetect(0.8);
 	$(window).resize(function() {
 		heightDetect();
 	});
