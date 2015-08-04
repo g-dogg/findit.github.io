f4c56eb2a75903b74614ee4f21160d8babb00617
g_dogg <gh.std.gg@gmail.com>
2015-08-04 23:12:53 +0600

    modified:   js/common.js

--- js/common.js
+++ js/common.js
@@ -34,6 +34,26 @@ $(document).ready(function() {
 		};
 	});
 
+/*
+
+	 var j = 0;
+	 var delay = 2000; //millisecond delay between cycles
+	 function cycleThru(){
+	         var jmax = $("ul#cyclelist li").length -1;
+	         $("ul#cyclelist li:eq(" + j + ")")
+	                 .animate({"opacity" : "1"} ,400)
+	                 .animate({"opacity" : "1"}, delay)
+	                 .animate({"opacity" : "0"}, 400, function(){
+	                         (j == jmax) ? j=0 : j++;
+	                         cycleThru();
+	                 });
+	         };
+
+	 cycleThru();
+
+
+*/
+
     $(".titleWrapper .logo").animated("fadeInDown", "bounceOut");
 	$(".titleWrapper h3").animated("fadeInDown", "fadeOutUp");
 	$(".about .abImg1").animated("flipInX", "flipOutX");
